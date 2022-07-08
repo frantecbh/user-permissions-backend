7bc5898220f0bea14e0925b822716fa837660cc5
frantecbh <franciscomenezesbh@gmail.com>
2022-07-08 00:38:11 -0300

    create table roules e permissions

---
+++ prisma/migrations/20220708033712_create_roules_e_permissions/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "roules" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "roules_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "pemissions" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "pemissions_pkey" PRIMARY KEY ("id")
+);
